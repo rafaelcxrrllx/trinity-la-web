48562a82bc25af42f42c7d4edb10baa2a0468861
Rafael Carrillo Muñoz <carrilr4@uci.edu>
2026-04-09 22:47:42 -0700

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
     base: '/trinity-la-web/',    
+    host: "::",
     port: 8080,
     hmr: {
       overlay: false,
